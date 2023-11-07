5ec5edf8e4e3156082c4b0465c47aecda5d03d70
faun3 <antonioculcea@gmail.com>
2023-11-07 18:27:57 +0200

    animate button click effect

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,10 +1,39 @@
-<script>
+<script lang="ts">
   import gsap from "gsap";
+  let buttonState = "closed";
+  import { onMount } from "svelte";
+  function transition() {
+    if (buttonState === "closed") {
+      buttonState = "open";
+      gsap.to(".expanding-menu div:nth-of-type(2)", {
+        duration: 0.5,
+        top: "0%",
+        ease: "power4.out",
+      });
+    } else {
+      buttonState = "closed";
+      gsap.to(".expanding-menu div:nth-of-type(2)", {
+        duration: 0.5,
+        top: "100%",
+        ease: "power4.out",
+      });
+    }
+  }
 </script>
 
 <nav class="navbar">
   <span class="">faun</span>
-  <button class="expanding-menu">menu</button>
+  <button
+    class={`expanding-menu ${buttonState}`}
+    on:click={transition}
+  >
+    <div>
+      <p>menu</p>
+    </div>
+    <div>
+      <p>close</p>
+    </div>
+  </button>
 </nav>
 
 <style lang="scss">
@@ -15,16 +44,35 @@
     padding: 1rem 2rem;
     align-items: center;
     min-height: 2rem;
-    background-color: black;
+    background-color: rgb(107, 103, 103);
     color: white;
   }
 
   .expanding-menu {
     border: none;
-    background-color: rgb(157, 244, 26);
+    background-color: rgb(211, 244, 26);
     color: black;
     border-radius: 999px;
-    padding: 0.5em 1em;
     text-transform: uppercase;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+
+    div {
+      width: 100%;
+      height: 100%;
+      padding: 0.5em 1.5em;
+    }
+
+    div:nth-of-type(2) {
+      background-color: black;
+      color: rgb(211, 244, 26);
+      position: absolute;
+      top: 100%;
+    }
   }
 </style>
